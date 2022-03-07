6ae50896a34c9d27d331cdfae8e763841107bf12
meet27 <meet27@umsystem.edu>
2022-03-07 08:56:20 -0600

    tar instead of gzip

--- scripts/create_tar_gz.py
+++ scripts/create_tar_gz.py
@@ -14,9 +14,9 @@ img_files = [f for f in listdir(source_dir) if isfile(join(source_dir, f))]
 for i in range(12):
     start = 100 * i
     stop = 100 * i + 100
-    output_filename = f"{output_dir}/images_{start+1}_{stop}.tgz"
+    output_filename = f"{output_dir}/images_{start+1}_{stop}.tar"
 
-    with tarfile.open(output_filename, "w:gz") as tar:
+    with tarfile.open(output_filename, "w") as tar:
         for img in img_files[start:stop]:
             tar.add(f"{source_dir}/{img}", arcname=img)
         tar.close()
