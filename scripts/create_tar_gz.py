3550df47367003d55e491ef6ddb25b72c600a292
meet27 <meet27@umsystem.edu>
2022-03-07 08:24:39 -0600

    changed file extension of tar.gz to tgz

--- scripts/create_tar_gz.py
+++ scripts/create_tar_gz.py
@@ -14,7 +14,7 @@ img_files = [f for f in listdir(source_dir) if isfile(join(source_dir, f))]
 for i in range(12):
     start = 100 * i
     stop = 100 * i + 100
-    output_filename = f"{output_dir}/images_{start+1}_{stop}.tar.gz"
+    output_filename = f"{output_dir}/images_{start+1}_{stop}.tgz"
 
     with tarfile.open(output_filename, "w:gz") as tar:
         for img in img_files[start:stop]:
